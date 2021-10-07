ae29224103bb8c0cefb90e54e527be4e5f683380
Aymane Benhima <aymanebenhima@gmail.com>
2021-10-07 19:05:55 +0100

    add dummy data

---
+++ data/Experience.js
@@ -0,0 +1,81 @@
+export const exp = [
+  {
+    id: "1",
+    startDate: "Dec 2016",
+    endDate: "Feb 2017",
+    jobTitle: "Network administrator",
+    company: "Province",
+    location: "Essaouira",
+    tasks: [
+      "Verified continuity of computer and telephone system services for users, assistance and project coordination using Asterisk for VoIP.",
+      "Monitored stability of network and wireless services.",
+      "Designed and implemented website using WordPress from initial concept, site architecture and wire frames to finished deliverable.",
+    ]
+  },
+  {
+    id: "2",
+    startDate: "Jun 2019",
+    endDate: "Sept 2019",
+    jobTitle: "Web developer",
+    company: "Mohammed VI Polytechnic University",
+    location: "Benguerir",
+    tasks: [
+      "Built marketplace platform from scratch",
+      "Employed coding practices based on commonly accepted standards to establish site layout and user interface.",
+      "Wrote lines of code using HTML, CSS, JavaScript and jQuery languages.",
+    ]
+  },
+  {
+    id: "3",
+    startDate: "Apr 2020",
+    endDate: "Sept 2020",
+    jobTitle: "Frontend web developer",
+    company: "Storeino",
+    location: "Agadir - Remotely",
+    tasks: [
+      "Built template builder like shopify, and built optimized stores in HTML and CSS, including NUXT JS integration and cross-browser compatibility, to support marketing.",
+      "Designed and updated layouts to meet usability and performance requirements",
+      "Worked in Agile-driven environment to effectively maintain project timelines and utilize available resources.",
+    ]
+  },
+  {
+    id: "3",
+    startDate: "Sept 2020",
+    endDate: "Nov 2020",
+    jobTitle: "Software developer",
+    company: "Bioxparc",
+    location: "Marrakech - Remotely",
+    tasks: [
+      "Planned and developed interfaces using angular that simplified overall management and offered ease-of-use.",
+      "Designed and updated layouts to meet usability and performance requirements.",
+      "Multi-tasked across multiple focuses to generate project results and meet deadlines and organizational expectations.",
+    ]
+  },
+  {
+    id: "4",
+    startDate: "Jan 2021",
+    endDate: "May 2021",
+    jobTitle: "Referent trainer - Web & Mobile Dev",
+    company: "Greenchip2.0 (powered by simplon)",
+    location: "Oujda",
+    tasks: [
+      "Facilitate the realization of the projects by the learners to create their own web apps and websites.",
+      "Propose corrections using best practice in web development on the projects carried out by the learners.",
+      "Supporting learners in obtaining the required certification to become developers.",
+    ]
+  },
+  {
+    id: "5",
+    startDate: "Jan 2019",
+    endDate: "Present",
+    jobTitle: "Full stack developer",
+    company: "Freelance",
+    location: "Worldwide",
+    tasks: [
+      "Provided front-end website development using new UI technologies.",
+      "Planned website development, converting mockups into usable web presence.",
+      "Developed functional digital design concepts across various platforms to strengthen company brand and identity.",
+    ]
+  },
+
+]
---
+++ data/projects.js
@@ -0,0 +1,306 @@
+const technos = {
+  html: {
+    name: "html5",
+    icon: "html.png",
+    size: "50px"
+  },
+  css: {
+    name: "css3",
+    icon: "css.png",
+    size: "50px"
+  },
+  js: {
+    name: "javascript",
+    icon: "js.png",
+    size: "50px"
+  },
+  ts: {
+    name: "typescript",
+    icon: "ts.png",
+    size: "50px"
+  },
+  scss: {
+    name: "scss",
+    icon: "scss.png",
+    size: "50px"
+  },
+  bootstrap: {
+    name: "bootstrap",
+    icon: "bootstrap.png",
+    size: "50px"
+  },
+  tailewindcss: {
+    name: "tailewindcss",
+    icon: "tailewindcss.png",
+    size: "50px"
+  },
+  pug: {
+    name: "pug",
+    icon: "pug.png",
+    size: "50px"
+  },
+  angular: {
+    name: "angular",
+    icon: "angular.png",
+    size: "50px"
+  },
+  vuejs: {
+    name: "vuejs",
+    icon: "vuejs.png",
+    size: "50px"
+  },
+  nuxtjs: {
+    name: "nuxtjs",
+    icon: "nuxtjs.png",
+    size: "50px"
+  },
+  reactjs: {
+    name: "reactjs",
+    icon: "reactjs.png",
+    size: "50px"
+  },
+  expressjs: {
+    name: "expressjs",
+    icon: "expressjs.png",
+    size: "50px"
+  },
+  ionic: {
+    name: "ionic",
+    icon: "ionic.png",
+    size: "50px"
+  },
+  php: {
+    name: "php",
+    icon: "php.png",
+    size: "50px"
+  },
+  laravel: {
+    name: "laravel",
+    icon: "laravel.png",
+    size: "50px"
+  },
+  mongodb: {
+    name: "mongodb",
+    icon: "mongodb.png",
+    size: "50px"
+  },
+  firebase: {
+    name: "firebase",
+    icon: "firebase.png",
+    size: "50px"
+  },
+  electronjs: {
+    name: "electronjs",
+    icon: "electronjs.png",
+    size: "50px"
+  },
+  jestjs: {
+    name: "jestjs",
+    icon: "jestjs.png",
+    size: "50px"
+  },
+  wordpress: {
+    name: "wordpress",
+    icon: "wordpress.png",
+    size: "50px"
+  },
+  docker: {
+    name: "docker",
+    icon: "docker.png",
+    size: "50px"
+  },
+  python: {
+    name: "python",
+    icon: "python.png",
+    size: "50px"
+  },
+  flutter: {
+    name: "flutter",
+    icon: "flutter.png",
+    size: "50px"
+  },
+}
+
+// Project card:::
+
+export const projects = [
+  {
+    id: "1",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "2",
+    name: "Landing page - Restaurant",
+    category:"frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "3",
+    name: "API - Devcamper",
+    category:"backend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.expressjs,
+      technos.mongodb,
+    ]
+  },
+  {
+    id: "4",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "5",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "6",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "7",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "8",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "9",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "10",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "11",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+  {
+    id: "12",
+    name: "Landing page - Web agency",
+    category:"Frontend",
+    mode: "development",
+    imgClass: "landing-page",
+    github: "url",
+    deployement: "url",
+    desc: "Creating UI landing page for my client to be present in web",
+    using: [
+      technos.nuxtjs,
+      technos.vuejs,
+      technos.bootstrap,
+    ]
+  },
+]
