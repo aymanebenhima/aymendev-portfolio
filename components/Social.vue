c5a81512d4eaad71fa876e72490b5cf929379a4c
Aymane Benhima <aymanebenhima@gmail.com>
2021-10-08 17:30:29 +0100

    add div to social

--- components/Social.vue
+++ components/Social.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="social">
-    <a href="https://www.github.com/aymanebenhima/" target="_blank" rel="noopener" title="Github">
-      <font-awesome-icon class="icon" :icon="['fab', 'github']" />
-    </a>
-    <a href="https://www.linkedin.com/in/aymanebenhima/" target="_blank" rel="noopener" title="Linkedin">
-      <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
-    </a>
-    <a href="https://twitter.com/aymanebenhima" target="_blank" rel="noopener" title="Twitter">
-      <font-awesome-icon class="icon" :icon="['fab', 'twitter']" />
-    </a>
-    <a class="mb-1" href="https://discordapp.com/users/<Aymen%20/>#7376" target="_blank" rel="noopener" title="<Aymen />#7376">
-      <font-awesome-icon class="icon" :icon="['fab', 'discord']" />
-    </a>
+    <div class="social__line">
+      <div class="social__items">
+        <a href="https://www.github.com/aymanebenhima/" target="_blank" rel="noopener" title="Github">
+          <font-awesome-icon class="icon" :icon="['fab', 'github']" />
+        </a>
+        <a href="https://www.linkedin.com/in/aymanebenhima/" target="_blank" rel="noopener" title="Linkedin">
+          <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
+        </a>
+        <a href="https://twitter.com/aymanebenhima" target="_blank" rel="noopener" title="Twitter">
+          <font-awesome-icon class="icon" :icon="['fab', 'twitter']" />
+        </a>
+        <a href="https://discordapp.com/users/<Aymen%20/>#7376" target="_blank" rel="noopener" title="<Aymen />#7376">
+          <font-awesome-icon class="icon" :icon="['fab', 'discord']" />
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,20 +45,47 @@ export default {
 <style lang="scss">
 .social {
   grid-column: social-start / social-end;
-  grid-row: 6 / -1;
-  border-right: .5px solid var(--text-color);
+  grid-row: 1 / -1;
+/*   border-right: .5px solid var(--text-color); */
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
-  align-items: center;
   z-index: 5;
 
+  &__line {
+    display: flex;
+    justify-content: space-evenly;
+
+      &::after {
+        content: "";
+        width: 1px;
+        height: 100%;
+        display: block;
+        background-color: var(--text-color);
+
+        @include respond(tab-port) {
+          display: none;
+        }
+    }
+    @include respond(tab-port) {
+      display: block;
+    }
+  }
+  &__items {
+    display: flex;
+    position: relative;
+    flex-direction: column;
+
+    @include respond(tab-port) {
+      flex-direction: row;
+      justify-content: space-evenly;
+      align-items: flex-end;
+    }
+  }
   @include respond(tab-port) {
     grid-column: 2 / span 1;
     grid-row: 4 / span 1;
-    flex-direction: row;
-    border-right: none;
-    justify-content: space-evenly;
+    display: block;
   }
 
   .icon {
