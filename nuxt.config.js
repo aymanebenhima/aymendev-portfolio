162a1b4b6b9838dd217a39735bde63bae24fa258
Aymane Benhima <aymanebenhima@gmail.com>
2021-10-10 20:16:50 +0100

    target server

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   // loading: '~/components/loading.vue',
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
