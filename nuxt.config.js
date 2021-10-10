d6ed531f9ccc5effb054bd6960da27d1d5ffde9f
Aymane Benhima <aymanebenhima@gmail.com>
2021-10-10 20:35:00 +0100

    nuxt axios baseURL

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,6 +77,22 @@ export default {
     }],
   ],
 
+  axios: {
+    baseURL: 'http://localhost:3000', // Used as fallback if no runtime config is provided
+  },
+
+  publicRuntimeConfig: {
+    axios: {
+      browserBaseURL: process.env.BROWSER_BASE_URL
+    }
+  },
+
+  privateRuntimeConfig: {
+    axios: {
+      baseURL: process.env.BASE_URL
+    }
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: ['vee-validate'],
